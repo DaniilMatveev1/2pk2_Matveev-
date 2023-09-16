ca91d59666c4c86255795dda81c0c9d6f3f56959
khamitov.ai2225 <khamitov.ai2225@COM409--R-5>
2023-09-16 07:18:40 +0300

    Добавьте файлы проекта.

---
+++ PZ_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace PZ_03
+{
+    internal class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            string caseSwitch = Console.ReadLine(); // программа которая в зависимости от характера ветра выдает сообщение о его скорости.
+
+            switch (caseSwitch)
+            {
+                case "слабый" :
+                    Console.WriteLine("слабый (1-4 м/с)"); // сильный ветер
+                    break;
+                case "умеренный":
+                    Console.WriteLine("умеренный (5-10 м/с)"); // умеренный ветер
+                    break;
+                case "сильный":
+                    Console.WriteLine("сильный (11-18 м/с)"); // сильный ветер
+                    break;
+                case "ураган":
+                    Console.WriteLine("ураган (больше 19 м/с)"); // ураган
+                    break;
+                default:
+                    Console.WriteLine("неверно указано"); // не одно из условий не подошло
+                    break;
+            }
+        }
+    }
+}
